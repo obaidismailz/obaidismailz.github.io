b7f0f7cf905b6551048e6edb3db3c6567eaf7dea
Obaid Ismail <obaidrock11@gmail.com>
2021-10-27 22:56:05 +0500

    QOD

---
+++ script.js
@@ -0,0 +1,12 @@
+    $.getJSON("https://quotes.rest/qod?language=en",
+            function (quotedata) {
+                console.log(quotedata);
+                var qodtitle = "Quote Of the Day: ";
+                qodtitle = qodtitle.bold();
+                // qodtitle = qodtitle.italics();
+                var quotestr = qodtitle + quotedata.contents.quotes[0].quote;
+                console.log(quotestr);
+                
+                $('.qod').append(quotestr.italics());
+            }   
+        );
