72a39b94904c16d39b01bc47a32f55bf45d3318c
Obaid Ismail <obaidrock11@gmail.com>
2022-09-13 08:36:24 +0500

    ok

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
             function (quotedata) {
                 console.log(quotedata);
                 var qodtitle = "Quote Of the Day: ";
+             //   var credits = "   (Sherry) ";
                 qodtitle = qodtitle.bold();
+                credits = credits.bold();
                 // qodtitle = qodtitle.italics();
-                var quotestr = qodtitle + quotedata.contents.quotes[0].quote;
+                var quotestr = qodtitle + quotedata.contents.quotes[0].quote + credits;
                 console.log(quotestr);
                 
                 $('.qod').append(quotestr.italics());
